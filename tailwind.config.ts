660ed6d72aac6412c14d9941a440d306ddf94f19
Magnus Stuart Juul <magnus.s.juul@gmail.com>
2024-04-13 13:56:09 +0200

    add additional custom duckling colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,15 @@ export default {
   theme: {
     extend: {
       colors: {
+        red: "#c62322",
+        green: "#009c1a",
+        black: "#000000",
         beige: "#dcd7cb",
+        grey: "#707070",
+        white: "#ffffff",
+        blue: "#097d94",
+        pink: "#d55698",
+        light_grey: "#f2f2f2",
       },
     },
   },
