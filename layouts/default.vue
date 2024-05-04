506757d721488be567bd34a8812d22cff4fd1355
Kasper Skov Hansen <56992010+kasperskovhansen@users.noreply.github.com>
2024-05-04 12:26:45 +0200

    Meta

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,18 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Duckling',
+  ogTitle: 'Duckling',
+  ogDescription: 'Duckling is documentary stories created by the next generation of journalists and storytellers.',
+  ogImage: 'https://www.duckling.co/assets/img/duckling_logo_text_right.png',
+  ogUrl: 'https://web.duckling.co/',
+  ogType: 'website',
+  ogSiteName: 'Duckling',
+  ogLocale: 'en_US',
+  twitterCard: 'summary_large_image',
+})
+</script>
 
 <style scoped></style>
