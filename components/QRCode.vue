c7fe0dc4ff6040cf6a6194b738f0314c40ef5d5b
Kasper Skov Hansen <56992010+kasperskovhansen@users.noreply.github.com>
2024-05-07 16:47:04 +0200

    Social sharing. QR. Styling

---
+++ components/QRCode.vue
@@ -0,0 +1,64 @@
+<template>
+    <div ref="qrCode"></div>
+</template>
+
+<script setup lang="ts">
+import type QRCodeStyling from 'qr-code-styling';
+
+const props = defineProps({
+    data: String
+});
+
+const { $qrCodeStyling } = useNuxtApp();
+const qrCode = ref<HTMLElement | null>(null);
+
+// Default options
+const options = {
+    width: 640,
+    height: 640,
+    // type: "svg",
+    data: props.data,
+    image: "https://images.squarespace-cdn.com/content/v1/62ff7317b7cfe25402e6d9b0/64247817-40c7-4277-a936-a9344da9511e/Logo.png?format=1500w",
+    dotsOptions: {
+        color: "#000",
+        type: "rounded"
+    },
+    backgroundOptions: {
+        color: "#fff",
+    },
+    imageOptions: {
+        crossOrigin: "anonymous",
+        margin: 3
+    }
+};
+
+const qrCodeStyling: QRCodeStyling = $qrCodeStyling(options);
+
+onMounted(() => {
+    // Append QR code to DOM element
+    qrCodeStyling.append(qrCode.value);
+    // Add viewbox to make it resizable
+    qrCode.value!.firstChild!.setAttribute('viewBox', '0 0 300 300');
+});
+
+watch(() => props.data, (newValue: string) => {
+    // Update QR code data when props change
+    options.data = newValue;
+    qrCodeStyling.update(options);
+    // Add viewbox to make it resizable
+    qrCode.value!.firstChild!.setAttribute('viewBox', '0 0 300 300');
+});
+</script>
+
+<style scoped>
+svg {
+    width: 100%;
+    height: 100%;
+}
+</style>
+<style>
+img {
+    width: 100%;
+    height: auto;
+}
+</style>
--- components/ShareDialog.vue
+++ components/ShareDialog.vue
@@ -1,14 +1,40 @@
 <template>
     <Teleport to="body">
         <!-- Emit closeDialog if click elsewhere -->
-        <div @click="closeDialog" class="fixed inset-0 z-0 bg-black bg-opacity-50"></div>
+        <div @click="closeDialog" class="fixed inset-0 z-0 flex items-center justify-center bg-black bg-opacity-50">
+        </div>
+        <div class="absolute top-0 left-0 flex items-center justify-center w-full h-full p-10 pointer-events-none ">
+            <div class="container flex flex-col items-stretch justify-center max-h-screen gap-8 md:max-w-screen-sm">
+                <div class="grid grid-cols-4 grid-rows-1 gap-4 grow ">
+                    <div v-for="social of socialShares"
+                        class="flex items-stretch justify-center overflow-hidden rounded-md cursor-pointer pointer-events-auto aspect-square">
+                        <SocialShare :network="social?.network" :url="social?.url" :key="social?.name" :styled="true"
+                            class="flex items-center justify-center grow " :label="false">
+                        </SocialShare>
+                    </div>
+                </div>
+                <div class="flex items-stretch justify-center w-full gap-2 pointer-events-auto">
+                    <button class="p-2 text-lg rounded-md bg-duckling_white" :class="{
+            'bg-green-500': didCopy
+        }" @click="onClickCopy">Copy</button>
+                    <input type="text" readonly class="p-1 text-lg rounded-md text grow" aria-expanded="true"
+                        v-model="url" />
+                </div>
+                <div class="flex flex-col items-stretch justify-center w-full overflow-hidden rounded-md">
+                    <QRCode class="flex flex-col items-stretch justify-center w-full pointer-events-auto" :data="url" />
+                </div>
+            </div>
+        </div>
     </Teleport>
-    <div
+    <!-- <div
         class="absolute left-0 z-10 flex flex-col items-start justify-center gap-1 p-1 m-auto mt-2 text-lg bg-white rounded-sm top-full">
         <div class="text-sm text-left text">Share on Social</div>
         <div v-if="socialShares?.length != 0" class="flex gap-2">
+            <QRCode :data="url" />
+            <button @click="downloadQRCode()">Download</button>
             <SocialShare v-for="social of socialShares" :network="social?.network" :url="social?.url"
-                :key="social?.name" :styled="true" :label="false" />
+                :key="social?.name" :styled="false" :label="false">
+            </SocialShare>
 
         </div>
         <div class="flex items-center justify-start gap-2">
@@ -18,7 +44,7 @@
             <input type="text" readonly class="p-1 text-xl border-2 text border-duckling_grey" aria-expanded="true"
                 v-model="url" />
         </div>
-    </div>
+    </div> -->
 </template>
 
 <script setup lang="ts">
@@ -48,7 +74,6 @@ const socialShares = [
         network: 'facebook',
         url: url.value,
         title: title.value,
-        user: 'twitter_user',
         hashtags: 'duckling,duck',
         image: coverImg.value,
     },
@@ -56,7 +81,6 @@ const socialShares = [
         network: 'twitter',
         url: url.value,
         title: title.value,
-        user: 'twitter_user',
         hashtags: 'duckling,duck',
         image: coverImg.value,
     },
@@ -64,7 +88,20 @@ const socialShares = [
         network: 'linkedin',
         url: url.value,
         title: title.value,
-        user: 'twitter_user',
+        hashtags: 'duckling,duck',
+        image: coverImg.value,
+    },
+    // {
+    //     network: 'messenger',
+    //     url: url.value,
+    //     title: title.value,
+    //     hashtags: 'duckling,duck',
+    //     image: coverImg.value,
+    // },
+    {
+        network: 'reddit',
+        url: url.value,
+        title: title.value,
         hashtags: 'duckling,duck',
         image: coverImg.value,
     },
@@ -77,4 +114,17 @@ const onClickCopy = () => {
     didCopy.value = navigator.clipboard.writeText(url.value);
 
 };
+
+// Function to download the QR Code
+function downloadQRCode () {
+    const { $qrCodeStyling } = useNuxtApp();
+    $qrCodeStyling.download();
+}
 </script>
+
+<style>
+.social-share-button__icon {
+    width: 4rem;
+    height: 4rem;
+}
+</style>
