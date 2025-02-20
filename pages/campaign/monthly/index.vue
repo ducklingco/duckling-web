e8abdd0bc0340b441cdf96522704602389d4f73a
Magnus Stuart Juul <magnus.s.juul@proton.me>
2025-02-20 12:44:49 +0100

    a

---
+++ pages/campaign/monthly/index.vue
@@ -0,0 +1,12 @@
+<!-- eslint-disable vue/html-self-closing -->
+<template>
+  <div class="absolute h-full w-full bg-duckling_beige">buen</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.custom-amount-input {
+  width: 100%;
+}
+</style>
