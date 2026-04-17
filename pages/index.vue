b7f4a7e4a49846e1481468a8a3d81ed8cf70f8c2
Bjarke Calvin <b@duckling.co>
2026-04-17 10:20:31 +0200

    Updating our link preview
    So it looks nice when we share a link to the page on SoMe and in chat

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
     <div class="fixed h-full w-full bg-duckling_beige" />
-      <div class="relative mx-auto px-4 pb-4 pt-8 md:pb-8">
+    <div class="container relative mx-auto px-4 pb-4 pt-8 md:pb-8 md:pt-14">
       <OverviewHeader class="pb-4 md:pb-10" />
-      <div class="container mx-auto flex-grow">
-        <DucksGrid />
-      </div>
+      <DucksGrid />
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Stories - Duckling — Free your story',
+  ogTitle: 'Stories - Duckling — Free your story',
+  description: 'Duckling is a home for real stories, shared by real people. Explore different perspectives and share your own.',
+  ogDescription: 'Duckling is a home for real stories, shared by real people. Explore different perspectives and share your own.',
+  ogImage: 'https://web.duckling.co/assets/img/linkpreview.png',
+  ogUrl: 'https://web.duckling.co',
+  ogType: 'website',
+  ogSiteName: 'Duckling',
+  twitterCard: 'summary_large_image',
+});
+</script>
